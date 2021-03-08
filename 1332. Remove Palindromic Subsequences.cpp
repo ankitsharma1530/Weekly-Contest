f91892a99e4ce9d48ce0f66c4f5acb70b6c9255f
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-03-08 18:14:47 +0530

    Create 1332. Remove Palindromic Subsequences.cpp

---
+++ 1332. Remove Palindromic Subsequences.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if(s.size()==0) 
+            return 0;
+        string temp=s;
+        reverse(temp.begin(),temp.end());
+        if(s==temp) 
+            return 1;
+        else
+            return 2;
+    }
+};
