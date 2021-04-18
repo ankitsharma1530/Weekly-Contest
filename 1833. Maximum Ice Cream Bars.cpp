a19f62760f9a3e396239cff49102a1cb062e3480
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-04-18 12:09:30 +0530

    Create 1833. Maximum Ice Cream Bars.cpp

---
+++ 1833. Maximum Ice Cream Bars.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int c=0;
+        for(int i=0;i<costs.size();i++)
+        {
+            if(coins>=costs[i])
+            {
+                c++;
+                coins-=costs[i];
+            }
+        }
+        return c;
+    }
+};
