dc8b874ea52a24379c24e3164c1984567e356043
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-04-26 13:50:30 +0530

    Create 1018. Binary Prefix Divisible By 5.cpp

---
+++ 1018. Binary Prefix Divisible By 5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& A) {
+        vector<bool>res;
+        int num =0;
+        for(int i=0;i<A.size();i++)
+        {
+            num = (num*2+A[i])%5;
+            if(num%5==0)
+            {
+                res.push_back(true);
+            }
+            else
+            {
+                res.push_back(false);
+            }
+        }
+        return res;
+    }
+};
