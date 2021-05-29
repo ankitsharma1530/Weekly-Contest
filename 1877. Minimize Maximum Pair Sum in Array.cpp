252ac21eaf87b1a34cfa23a92766d490e0d183ca
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-05-29 22:37:12 +0530

    Create 1877. Minimize Maximum Pair Sum in Array.cpp

---
+++ 1877. Minimize Maximum Pair Sum in Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int m = INT_MIN;
+        int i=0;
+        int j = nums.size()-1;
+        while(i<j){
+            m = max(m,nums[i]+nums[j]);
+            i++;
+            j--;
+        }
+        return m;
+    }
+};
